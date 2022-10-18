5703fcb4b784c63948834c6d75e9c8555341d2e3
manq2010 <manq2010@gmail.com>
2022-10-18 13:23:46 +0200

    Add sytling for mobile version

--- js/index.js
+++ js/index.js
@@ -1,94 +1,51 @@
 // Mobile Menu
-const navbar = document.getElementById('navbar');
-const mobileMenu = document.createElement('div');
-mobileMenu.classList.add('hamburger-menu');
-navbar.append(mobileMenu);
-// const test = document.querySelector('.modal-btn');
+const humburger = document.querySelector('.btn');
+const closeBtn = document.querySelector('.btn-close');
+console.log(humburger);
+const mobileMenu = document.querySelector('.main-nav');
+const mobileNav = document.querySelector('.mobile-nav');
 
-const humburger = `
-<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor"
-    class="bi bi-list text-dark modal-btn" viewBox="0 0 16 16">
-    <path fill-rule="evenodd"
-        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
-</svg> `;
 
-mobileMenu.insertAdjacentHTML('afterbegin', humburger);
+function mobileMenuToggle() {
+    mobileNav.classList.toggle('open');
+    humburger.classList.toggle('close');
+    closeBtn.classList.toggle('open');
+    mobileMenu.classList.toggle('open');
+}
 
-const cardModalMenu = `
-    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-x-lg"
-        viewBox="0 0 16 16">
-        <path
-            d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
-    </svg> -->
-
-    <button type="button" class="btn-close"></button>
-    
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Show</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Join</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Sponsor</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">News</a>
-        </li>
-    </ul>
-`;
-
-mobileMenu.addEventListener('click', () => {
-    const modalMenu = document.createElement('div');
-    modalMenu.classList.add('mobile-menu');
-    modalMenu.insertAdjacentHTML('afterbegin', cardModalMenu);
-    navbar.append(modalMenu);
-
-    const closeMenu = document.querySelector('.btn-close');
-
-    closeMenu.addEventListener('click', () => {
-        console.log('clicked');
-        navbar.remove(modalMenu);
-    });
-});
+humburger.addEventListener('click', mobileMenuToggle);
+closeBtn.addEventListener('click', mobileMenuToggle);
 
 // Main Program
 
 const programCard = [
     {
         id: 0,
-        cardImage: ['mic-fill', 70, 70],
+        cardImage: 'mic-fill',
         cardName: 'Mic',
         cardDetails: 'Upcoming comedians will get to showcase their skills',
     },
     {
         id: 1,
-        cardImage: ['egg-fried', 70, 70],
+        cardImage: 'egg-fried',
         cardName: 'Food',
         cardDetails: 'A buffet menu will we available at venue',
     },
     {
         id: 2,
-        cardImage: ['building', 70, 70],
+        cardImage: 'building',
         cardName: 'Hotel',
         cardDetails: 'Accommodation will be after the event, free for vvip ticket holders',
     },
     {
         id: 3,
-        cardImage: ['car-front-fill', 70, 70],
+        cardImage: 'car-front-fill',
         cardName: 'Uber',
         cardDetails: 'Transport with be avlaible a at a discount',
     },
     {
         id: 4,
-        cardImage: ['people-fill', 70, 70],
+        cardImage: 'people-fill',
         cardName: 'Community',
         cardDetails: 'You will have interestion with a wider network',
     },
@@ -99,7 +56,7 @@ let card = '';
 function contentCard(program) {
     const cards = `
  <div class="card-holder">
- <img src="/assets/${program.cardImage[0]}.svg" alt="${program.cardImage[0]}" width="${program.cardImage[1]}" height="${program.cardImage[2]}">
+ <img src="/assets/${program.cardImage}.svg" alt="${program.cardImage}" width="${40}" height="${40}">
  <h3 class="card-title">${program.cardName}</h3>
  <p class="card-details">${program.cardDetails}</p>
 </div>
@@ -151,9 +108,7 @@ const speakerCard = [
     },
 ];
 
-
 function contentSpeakers(speaker) {
-
     const cards = `
     <div class="speaker-holder">
         <img src="/assets/${speaker.image}.svg" alt="${speaker.image}" width="${20}" height="${20}">
@@ -191,7 +146,7 @@ moreSpeaker.addEventListener('click', () => {
             moreCardSpeaker += contentSpeakers(speaker);
         }
     });
-    console.log(moreCardSpeaker);
+    // console.log(moreCardSpeaker);
     speakerContainer.insertAdjacentHTML('afterbegin', moreCardSpeaker);
 
     moreSpeaker.style.display = 'none';
